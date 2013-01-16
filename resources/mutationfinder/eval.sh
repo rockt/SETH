37187b303b6075b3e4df39b6a5ba9a70229f4904
Philippe <philippe@kunterbunt.(none)>
2013-01-16 16:46:36 +0100

    eval.sh no longer needed

--- resources/mutationfinder/eval.sh
+++ resources/mutationfinder/eval.sh
@@ -1,7 +1,9 @@
+#Apply original MutationFinder on the training and test-corpus
 python origDist/mutation_finder.py -r origDist/regex.txt corpus/devo_set.txt -o predictions/
 python origDist/mutation_finder.py -r origDist/regex.txt corpus/test_set.txt -o predictions/
 
 
-#Eval..
-python origDist/performance.py predictions/devo_set.txt.seth  corpus/devo_gold_std.txt
-python origDist/performance.py predictions/test_set.txt.seth  corpus/test_gold_std.txt
+#Evaluate original Mutationfinder
+python origDist/performance.py predictions/devo_set.txt.mf  corpus/devo_gold_std.txt
+
+python origDist/performance.py predictions/test_set.txt.mf  corpus/test_gold_std.txt
