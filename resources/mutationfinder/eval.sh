a38b1fbd33a2b7dcba077d810a2f83f0d19e3aef
Philippe <thomas@informatik.hu-berlin.de>
2013-01-17 10:09:33 +0100

    deleted eval

--- resources/mutationfinder/eval.sh
+++
@@ -1,9 +0,0 @@
-#Apply original MutationFinder on the training and test-corpus
-python origDist/mutation_finder.py -r origDist/regex.txt corpus/devo_set.txt -o predictions/
-python origDist/mutation_finder.py -r origDist/regex.txt corpus/test_set.txt -o predictions/
-
-
-#Evaluate original Mutationfinder
-python origDist/performance.py predictions/devo_set.txt.mf  corpus/devo_gold_std.txt
-
-python origDist/performance.py predictions/test_set.txt.mf  corpus/test_gold_std.txt
