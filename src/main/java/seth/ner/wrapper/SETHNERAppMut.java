7f7f304c7311b94ab7019de3a130104deb822eb3
yvonne <yvonne.mayer@fu-berlin.de>
2013-10-09 14:05:33 +0200

    reverted changes in SETH.java, SETHNERAppMut.java, SETHSuite.scala, seth.iml

--- src/main/java/seth/ner/wrapper/SETHNERAppMut.java
+++ src/main/java/seth/ner/wrapper/SETHNERAppMut.java
@@ -1,7 +1,6 @@
 package seth.ner.wrapper;
 
 import de.hu.berlin.wbi.objects.MutationMention;
-import edu.uchsc.ccp.nlp.ei.mutation.MutationException;
 import seth.SETH;
 
 import java.util.List;
@@ -23,19 +22,4 @@ public class SETHNERAppMut {
         }
         System.out.println("Extracted " + result.size() + " mutations.");
     }
-//    public static void main(String[] args) throws MutationException {
-//
-//        SETH seth = new SETH("resources/mutations.txt",false, true);
-//
-//        // (23605251,47,6633,6643,'482.xx-483','482.xx-483',NULL,'','','','','COPY_NUMBER_VARIATION',6489,6497,'Pneumonia','C0032285','HP:0006532',NULL)
-//
-//        List<MutationMention> mentions=
-//                seth.findMutations("482.xx-483"); //R403stop Glu99X E590 K Tyr22Xc.861insG p.990delM
-//
-//        for(MutationMention mention : mentions){
-//            System.out.println(mention);
-//            System.out.println(mention.toNormalized());
-//        }
-//
-//}
 }
