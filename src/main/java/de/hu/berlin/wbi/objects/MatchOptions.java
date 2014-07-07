13420669a14b2ac8e2b7fb8fd4e73a5ae9556e75
philippe <thomas@informatik.hu-berlin.de>
2014-07-07 18:36:58 -0400

    Removed the very confusing RESIDUE enumeration

--- src/main/java/de/hu/berlin/wbi/objects/MatchOptions.java
+++ src/main/java/de/hu/berlin/wbi/objects/MatchOptions.java
@@ -1,13 +1,11 @@
 package de.hu.berlin.wbi.objects;
 
 public enum MatchOptions {
-    // Flag if the mutation has been normalized to protein-sequence, false if nucleotide-sequence
+    // Has been normalized to protein sequence? Otherwise nucleotide
     PSM,
-    // True when the wild type and mutated residues match
-    RESIDUES,
-    // True when the locations are equal
+    // Location matches exactly?
     LOC,
-    // Mention is derived one position after leading methionine.
+    // Location matches by methionine
     METHIONE,
     // Wildtype and Mutated alleles are swapped (e.g. Ala123Tyr -> Tyr123Ala)
     SWAPPED
