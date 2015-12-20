293cd61626bd0a7a8f47e86cefb392398871d650
philippe <thomas@informatik.hu-berlin.de>
2015-12-20 15:54:44 +0100

    Resolved a bug which had problems comparing two MutationMentions when one (or more values) of a mutation where null

--- src/main/java/seth/SETH.java
+++ src/main/java/seth/SETH.java
@@ -20,39 +20,48 @@ import java.util.*;
  * SNPs written in free text (e.g. Ala to Tyr substitution at position 123),
  * SNPs written in deprecated nomenclature
  * and dbSNP mentions
- * @author  Philippe Thomas
+ *
+ * @author Philippe Thomas
  */
 public class SETH {
 
 
-    /** SETH object for recognizing nomenclature SNPs*/
+    /**
+     * SETH object for recognizing nomenclature SNPs
+     */
     private final SETHNER seth;
 
-    /** MutationFinder object for recognizing free-text Substitutions*/
+    /**
+     * MutationFinder object for recognizing free-text Substitutions
+     */
     private final MutationFinder mf;
 
-    /** Set of regular expressions for finding free text deletions, insertions, etc.*/
+    /**
+     * Set of regular expressions for finding free text deletions, insertions, etc.
+     */
     private final OldNomenclature bl;
 
-    /** Detects dbSNP mentions (e.g., rs334) */
+    /**
+     * Detects dbSNP mentions (e.g., rs334)
+     */
     private final dbSNPRecognizer snpRecognizer;
 
     /**
      * Initializes {@link MutationFinder} and {@link SETHNER}.
      * Requires a file with regular expressions for MutationFinder
      *
-     * @param regexFile File location with regular expressions for MutationFinder
-     * @param exactGrammar   If true, SETH uses the exact EBNF grammar, otherwise matching is fuzzy
+     * @param regexFile       File location with regular expressions for MutationFinder
+     * @param exactGrammar    If true, SETH uses the exact EBNF grammar, otherwise matching is fuzzy
      * @param oldNomenclature If true, SETH searches for deletions, insertions, frameshifts in old nomenclature
      */
-    public SETH(String regexFile, boolean exactGrammar, boolean oldNomenclature){
+    public SETH(String regexFile, boolean exactGrammar, boolean oldNomenclature) {
         super();
 
         this.mf = new MutationFinder(regexFile);
         this.seth = new SETHNER(exactGrammar);
         this.snpRecognizer = new dbSNPRecognizer();
 
-        if(oldNomenclature)
+        if (oldNomenclature)
             this.bl = new OldNomenclature();
         else
             this.bl = null;
@@ -62,10 +71,11 @@ public class SETH {
 
     /**
      * Searches for mutation mentions in applied text and returns them
+     *
      * @param text Text possibly containing a mutation mention
      * @return List of {@link MutationMention}
      */
-    public List<MutationMention> findMutations(String text){
+    public List<MutationMention> findMutations(String text) {
         Set<MutationMention> mutations = new HashSet<MutationMention>();
 
         //Extract variations following the latest HGVS nomenclature
@@ -75,33 +85,33 @@ public class SETH {
         mutations.addAll(snpRecognizer.extractMutations(text));
 
         //Extracts variations following different Nomenclature forms
-        if(bl != null)
+        if (bl != null)
             mutations.addAll(bl.extractMutations(text));
 
         //Extract mutations, using a modified version of MutationFinder
-       try {
+        try {
             Map<edu.uchsc.ccp.nlp.ei.mutation.Mutation, Set<int[]>> map = mf.extractMutations(text);
 
-           for(edu.uchsc.ccp.nlp.ei.mutation.Mutation mutation : map.keySet()){
-               PointMutation pm = (PointMutation) mutation;
+            for (edu.uchsc.ccp.nlp.ei.mutation.Mutation mutation : map.keySet()) {
+                PointMutation pm = (PointMutation) mutation;
 
-               for(int [] location : map.get(mutation)){
-                   String originalMatch =  text.substring(location[0], location[1]);
+                for (int[] location : map.get(mutation)) {
+                    String originalMatch = text.substring(location[0], location[1]);
 
-                   // MutationFinder only returns protein matches
-                   MutationMention tmpMutation =    new MutationMention(location[0], location[1], originalMatch, null, pm.getPosition(),
-                           String.valueOf(pm.getWtResidue()), String.valueOf(pm.getMutResidue()),
-                           Type.SUBSTITUTION, MutationMention.Tool.MUTATIONFINDER);
-                   tmpMutation.setPatternId(pm.getId());
-                   if(pm.isMatchesLongForm()){
-                       tmpMutation.setPsm(true);
-                       tmpMutation.setAmbiguous(false);
-                       tmpMutation.setNsm(false);
-                   }
+                    // MutationFinder only returns protein matches
+                    MutationMention tmpMutation = new MutationMention(location[0], location[1], originalMatch, null, pm.getPosition(),
+                            String.valueOf(pm.getWtResidue()), String.valueOf(pm.getMutResidue()),
+                            Type.SUBSTITUTION, MutationMention.Tool.MUTATIONFINDER);
+                    tmpMutation.setPatternId(pm.getId());
+                    if (pm.isMatchesLongForm()) {
+                        tmpMutation.setPsm(true);
+                        tmpMutation.setAmbiguous(false);
+                        tmpMutation.setNsm(false);
+                    }
 
-                   mutations.add(tmpMutation);
-               }
-           }
+                    mutations.add(tmpMutation);
+                }
+            }
 
         } catch (MutationException e) {
             e.printStackTrace();
@@ -113,26 +123,34 @@ public class SETH {
         //Some mentions can be found be different tools. Thus, we remove all duplicates by preserving the longest element
         //E.g., IVS2 + 1G>A is recognized  Mutationfinder (2+1G>A) and correctly Tool.Regex
         List<MutationMention> result = new ArrayList<MutationMention>(mutations.size());
-        for(MutationMention mm : mutations){
+        for (MutationMention mm : mutations) {
 
             boolean contained = false;
-            loop:for(MutationMention m : result){
+            loop:for (MutationMention m : result) {
 
                 //This variable is needed to check  whether two mutations are identical. (Important as MF finds overlapping ,mutations Trp-64 to Phe or Tyr)
                 boolean equal = false;
-                try{
-                    equal =  mm.getPosition().equals(m.getPosition()) && mm.getMutResidue().equals(m.getMutResidue()) && mm.getWtResidue().equals(m.getWtResidue());
-                }catch(NullPointerException npe){}      //Some mutations fave no residues
 
+                //Debugging purpose
+                //System.out.println("'" + mm.getPosition() + "' -- '" + m.getPosition() + "'");
+                //System.out.println("'" + mm.getMutResidue() + "' -- '" + m.getMutResidue() + "'");
+                //System.out.println("'" + mm.getWtResidue() + "' -- '" + m.getWtResidue() + "'");
+
+
+                //This potentially lead to problematic null pointer exceptions
+                //equal = mm.getPosition().equals(m.getPosition()) && mm.getMutResidue().equals(m.getMutResidue()) && mm.getWtResidue().equals(m.getWtResidue());
+
+
+                //Better handling of potentially null values
+                equal =  Objects.equals(mm.getPosition(), m.getPosition()) && Objects.equals(mm.getMutResidue(), m.getMutResidue()) && Objects.equals(mm.getWtResidue(), m.getWtResidue());
 
                 //In case the two recognized mutation mentions are equally long Tool.DBSNP wins over Tool.SETH  (both tools find mentions like rs123:A>T)
-                if(mm.getStart() == m.getStart() && mm.getEnd() == m.getEnd()){
+                if (mm.getStart() == m.getStart() && mm.getEnd() == m.getEnd()) {
 
-                    if(mm.getTool() == MutationMention.Tool.SETH && m.getTool() == MutationMention.Tool.DBSNP){
+                    if (mm.getTool() == MutationMention.Tool.SETH && m.getTool() == MutationMention.Tool.DBSNP) {
                         contained = true;
                         break loop;
-                    }
-                    else if(m.getTool() == MutationMention.Tool.SETH && mm.getTool() == MutationMention.Tool.DBSNP){
+                    } else if (m.getTool() == MutationMention.Tool.SETH && mm.getTool() == MutationMention.Tool.DBSNP) {
                         result.remove(m);
                         break loop;
                     }
@@ -140,19 +158,19 @@ public class SETH {
                 }
 
                 //If the new mention is smaller than the mention contained in the result, ignore the smaller one
-                else if(mm.getStart() >= m.getStart() && mm.getEnd() <= m.getEnd() && equal){
+                else if (mm.getStart() >= m.getStart() && mm.getEnd() <= m.getEnd() && equal) {
                     contained = true;
                     break loop;
                 }
 
                 //If the new mention is longer, remove the old (smaller) mention and add the new one
-                else if(m.getStart() >= mm.getStart() && m.getEnd() <= mm.getEnd()  && equal){
+                else if (m.getStart() >= mm.getStart() && m.getEnd() <= mm.getEnd() && equal) {
                     result.remove(m);
                     break loop;
                 }
             }
 
-            if(!contained)
+            if (!contained)
                 result.add(mm);
 
         }
@@ -163,29 +181,29 @@ public class SETH {
 
     /**
      * Minimal example to perform named entity recognition and normalization using SETH
+     *
      * @param args
      * @throws SQLException
      * @throws IOException
      */
-    public static void main(String[] args)throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException, IOException {
         String text = "p.A123T and Val158Met";
 
         /** Part1: Recognition of mutation mentions */
         SETH seth = new SETH("resources/mutations.txt", true, true);
         List<MutationMention> mutations = seth.findMutations(text);
-        try{
-            for(MutationMention mutation : mutations){
+        try {
+            for (MutationMention mutation : mutations) {
                 System.out.println(mutation.toNormalized());
             }
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
-       catch(Throwable e){
-              e.printStackTrace();
-       }
 
 
-       /** Part 2: Normalization of mutation mentions to dbSNP
-        * This part communicates with a local database dump
-       */
+        /** Part 2: Normalization of mutation mentions to dbSNP
+         * This part communicates with a local database dump
+         */
         final Properties property = new Properties();
         property.loadFromXML(new FileInputStream(new File("myProperty.xml"))); //Load property file
         final DatabaseConnection mysql = new DatabaseConnection(property);
@@ -194,21 +212,20 @@ public class SETH {
         dbSNP.init(mysql, property.getProperty("database.PSM"), property.getProperty("database.hgvs_view"));
         UniprotFeature.init(mysql, property.getProperty("database.uniprot"));
 
-        int gene = 1312;	//Entrez Gene ID associated with the current sentence
-        final List<dbSNP> potentialSNPs = dbSNP.getSNP(gene);	//Get a list of dbSNPs which could potentially represent the mutation mention
+        int gene = 1312;    //Entrez Gene ID associated with the current sentence
+        final List<dbSNP> potentialSNPs = dbSNP.getSNP(gene);    //Get a list of dbSNPs which could potentially represent the mutation mention
         final List<UniprotFeature> features = UniprotFeature.getFeatures(gene);    //Get all associated UniProt features
 
-        for(MutationMention mutation : mutations){
+        for (MutationMention mutation : mutations) {
             System.out.println(mutation);
             mutation.normalizeSNP(potentialSNPs, features, false);
-            List<dbSNPNormalized> normalized = mutation.getNormalized();	//Get list of all dbSNP entries with which I could successfully associate the mutation
+            List<dbSNPNormalized> normalized = mutation.getNormalized();    //Get list of all dbSNP entries with which I could successfully associate the mutation
 
             // Print information
-            for(dbSNPNormalized snp : normalized){
-                System.out.println(mutation +" --- rs" +snp.getRsID());
+            for (dbSNPNormalized snp : normalized) {
+                System.out.println(mutation + " --- rs" + snp.getRsID());
             }
         }
-
 
 
     }
