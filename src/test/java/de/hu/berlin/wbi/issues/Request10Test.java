50dc6c2411243c9e02be6bf521056c824af48b86
Erechtheus <thomas@informatik.hu-berlin.de>
2016-11-05 23:19:31 +0100

    Refactored tests to explain the problems

--- src/test/java/de/hu/berlin/wbi/issues/Request10Test.java
+++ src/test/java/de/hu/berlin/wbi/issues/Request10Test.java
@@ -37,15 +37,15 @@ public class Request10Test extends TestCase {
         List<MutationMention> mutationMentions = seth.findMutations(text);
 
         assertTrue(mutationMentions.size() > 0); //Do we actually find a mutation?
-
         List<String> recognized = mutationMentions.stream().map(a -> a.getText()).collect(Collectors.toList());
+        System.out.println(recognized);
         assertTrue(recognized.contains(mutation)); //Check if the mutation is correctly recognized
     }
 
 
     @Test
     public void testPmid20806047() throws Exception {
-        assertFoundMutation("A novel dominant mutation at the stop codon of FOXE3, c.959G>C (p.X320SerextX72), was identified in a patient with congenital cataract",
+        assertFoundMutation("A novel dominant mutation at the stop codon of FOXE3, c.959G>C (p.X320SerextX72)",
                 "p.X320SerextX72");
     }
 
@@ -57,31 +57,35 @@ public class Request10Test extends TestCase {
 
     @Test
     public void testPmid22907560() throws Exception {
+        //Hys is no valid amino acid
         assertFoundMutation("predicted to cause a p.Arg313Hys amino acid change.", "p.Arg313Hys");
     }
 
     @Test
     public void testPmid18486607() throws Exception {
+        //Fixed
         assertFoundMutation("p.Arg315Stop", "p.Arg315Stop");
     }
 
     @Test
     public void testPmid23017188() throws Exception {
-        assertFoundMutation("Identification of the CFTR p.Phe508Del founder mutation in the absence of a polythymidine 9T allele in a Hispanic patient.", "p.Phe508Del");
+        //Del with upper case
+        assertFoundMutation("Identification of the CFTR p.Phe508Del p.Phe508Ins founder ", "p.Phe508Del");
     }
 
     @Test
     public void testPmid24158885() throws Exception {
+        //Amino acids with different upper/lower case
         assertFoundMutation("p.Met694IIe", "p.Met694IIe");
     }
 
     @Test
     public void testPmid23856132() throws Exception {
-        assertFoundMutation("The predominant mutation in this population sample was p.R261Q G>A, p.F55>Lfs and p.R243Q G>A.", "p.F55>Lfs");
+        assertFoundMutation("The predominant mutation in this population sample was p.F55>Lfs ", "p.F55>Lfs");
     }
 
     @Test
     public void testPmid18708425() throws Exception {
-        assertFoundMutation("We report an insertion of two leucines (p.L21tri also designated p.L15_L16ins2L) in the leucine stretch of the signal peptide of PCSK9 that is found in two of 25 families with familial combined hyperlipidaemia (FCHL).", "p.L15_L16ins2L");
+        assertFoundMutation("We report an insertion of two leucines (p.L21tri also designated p.L15_L16ins2L) ", "p.L15_L16ins2L");
     }
 }
