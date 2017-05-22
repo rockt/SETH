58e11c0f6f482dfbba9898f9bea4d4a9563d532f
philippe-thomas <philippe.thomas@dfki.de>
2017-05-22 16:59:50 +0200

    Refactored tests

--- src/test/java/de/hu/berlin/wbi/issues/IssuesNalaTest.java
+++ src/test/java/de/hu/berlin/wbi/issues/IssuesNalaTest.java
@@ -23,15 +23,15 @@ public class IssuesNalaTest {
 
     @Test
     public void testExample1() throws Exception {
-        List<MutationMention> mutationMentions = seth.findMutations("c.388+3insT  c.388+3delT and c.35delG and c.35insG");
-        System.out.println(mutationMentions);
+        List<MutationMention> mutationMentions = seth.findMutations("c.31+3insT");
+        //System.out.println(mutationMentions);
         Assert.assertEquals(mutationMentions.size(), 1); //Do we actually find a mutation?
     }
 
     @Test //Done
     public void testExample2() throws Exception {
         List<MutationMention> mutationMentions = seth.findMutations("delPhe1388");
-        System.out.println(mutationMentions);
+        //System.out.println(mutationMentions);
         Assert.assertEquals(mutationMentions.size(), 1); //Do we actually find a mutation?
 
     }
@@ -39,7 +39,7 @@ public class IssuesNalaTest {
     @Test
     public void testExample3() throws Exception {
         List<MutationMention> mutationMentions = seth.findMutations("G643 to A ");
-        System.out.println(mutationMentions);
+        //System.out.println(mutationMentions);
         Assert.assertEquals(mutationMentions.size(), 1); //Do we actually find a mutation?
 
     }
@@ -47,7 +47,7 @@ public class IssuesNalaTest {
     @Test //Fixed
     public void testExample4() throws Exception {
         List<MutationMention> mutationMentions = seth.findMutations("glycine was substituted by lysine at residue 18");
-        System.out.println(mutationMentions);
+        //System.out.println(mutationMentions);
         Assert.assertEquals(mutationMentions.size(), 1); //Do we actually find a mutation?
 
     }
