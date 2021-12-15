65f1701f4094dc4e94c14e160747eb8c7e473edf
Philippe Thomas <philippe.thomas@dfki.de>
2021-12-15 15:29:41 +0100

    Still debugging

--- src/main/java/de/hu/berlin/wbi/stuff/dbSNPParser/json/CountLines.java
+++ src/main/java/de/hu/berlin/wbi/stuff/dbSNPParser/json/CountLines.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 public class CountLines {
     public static void main(String[] args) throws IOException, CompressorException {
-        String jsonFolder = "/media/philippe/Elements/SETH/dbSNP/";
+        String jsonFolder = "JSON/";
 
 
         File[] files = new File(jsonFolder).listFiles();
